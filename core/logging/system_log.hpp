bf24e9d4ac8ada2ac29989de07222ca30ba4ab7c
Joshua Booth <joshua@icoteq.com>
2020-11-24 17:59:19 +0000

    system_log: fix copy paste error

--- core/logging/system_log.hpp
+++ core/logging/system_log.hpp
@@ -15,28 +15,28 @@ public:
 		CircularFile *f;
 		try {
 			// See if an existing file exists
-			f = new CircularFile(main_filesystem, SystemLog::filename, LFS_O_RDONLY, SensorLog::MAX_SIZE);
+			f = new CircularFile(main_filesystem, SystemLog::filename, LFS_O_RDONLY, SystemLog::MAX_SIZE);
 		} catch (int e) {
 			// Create a new log file if the file does not exist
-			f = new CircularFile(main_filesystem, SystemLog::filename, LFS_O_WRONLY | LFS_O_CREAT, SensorLog::MAX_SIZE);
+			f = new CircularFile(main_filesystem, SystemLog::filename, LFS_O_WRONLY | LFS_O_CREAT, SystemLog::MAX_SIZE);
 		}
 		delete f;
 	}
 
 	static void write(void *entry) {
-		CircularFile f(main_filesystem, SystemLog::filename, LFS_O_WRONLY, SensorLog::MAX_SIZE);
+		CircularFile f(main_filesystem, SystemLog::filename, LFS_O_WRONLY, SystemLog::MAX_SIZE);
 		f.write(entry, (lfs_size_t)sizeof(LogEntry));
 	}
 
 	static void read(void *entry, int index=0) {
-		CircularFile f(main_filesystem, SystemLog::filename, LFS_O_RDONLY, SensorLog::MAX_SIZE);
+		CircularFile f(main_filesystem, SystemLog::filename, LFS_O_RDONLY, SystemLog::MAX_SIZE);
 		if (index != 0)
 			f.seek(f.m_offset + index * (sizeof(LogEntry)));
 		f.read(entry, (lfs_size_t)sizeof(LogEntry));
 	}
 
 	static unsigned int num_entries() {
-		CircularFile f(main_filesystem, "sensor.log", LFS_O_RDONLY, SensorLog::MAX_SIZE);
+		CircularFile f(main_filesystem, "sensor.log", LFS_O_RDONLY, SystemLog::MAX_SIZE);
 		return f.size() / sizeof(LogEntry);
 	}
 };
