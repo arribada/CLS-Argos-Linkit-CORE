4306fb7a0f3e3bf7830ab4843d4ebe5544f6965f
Joshua Booth <joshua@icoteq.com>
2020-11-24 17:55:13 +0000

    logging: resolve compilation errors and use safer vsnprintf variant

--- core/logging/logger.hpp
+++ core/logging/logger.hpp
@@ -20,55 +20,55 @@ enum LogLevel {
 class Logger {
 
 private:
-	static int log_level = LOG_LEVEL_DEBUG;
+	static int m_log_level = LOG_LEVEL_DEBUG;
 
 public:
 	static void set_log_level(int level) {
-		log_level = level;
+		m_log_level = level;
 	}
 	static void warn(const char *msg, ...) {
-		if (log_level >= LOG_LEVEL_WARN) {
+		if (m_log_level >= LOG_LEVEL_WARN) {
 			LogEntry buffer;
 			va_list args;
-			va_start (args, msg);
-			vsprintf (buffer.data, msg, args);
-			va_end (args);
+			va_start(args, msg);
+			vsnprintf(reinterpret_cast<char*>(buffer.data), sizeof(buffer.data), msg, args);
+			va_end(args);
 			buffer.header.log_type = LOG_WARN;
 			// TODO: set date time in header
 			write(&buffer);
 		}
 	}
 	static void error(const char *msg, ...) {
-		if (log_level >= LOG_LEVEL_ERROR) {
+		if (m_log_level >= LOG_LEVEL_ERROR) {
 			LogEntry buffer;
 			va_list args;
-			va_start (args, msg);
-			vsprintf (buffer.data, msg, args);
-			va_end (args);
+			va_start(args, msg);
+			vsnprintf(reinterpret_cast<char*>(buffer.data), sizeof(buffer.data), msg, args);
+			va_end(args);
 			buffer.header.log_type = LOG_ERROR;
 			// TODO: set date time in header
 			write(&buffer);
 		}
 	}
 	static void info(const char *msg, ...) {
-		if (log_level >= LOG_LEVEL_INFO) {
+		if (m_log_level >= LOG_LEVEL_INFO) {
 			LogEntry buffer;
 			va_list args;
-			va_start (args, msg);
-			vsprintf (buffer.data, msg, args);
-			va_end (args);
+			va_start(args, msg);
+			vsnprintf(reinterpret_cast<char*>(buffer.data), sizeof(buffer.data), msg, args);
+			va_end(args);
 			buffer.header.log_type = LOG_INFO;
 			// TODO: set date time in header
 			write(&buffer);
 		}
 	}
 	static void trace(const char *msg, ...) {
-		if (log_level >= LOG_LEVEL_INFO) {
+		if (m_log_level >= LOG_LEVEL_INFO) {
 			LogEntry buffer;
 			va_list args;
-			va_start (args, msg);
-			vsprintf (buffer.data, msg, args);
-			va_end (args);
+			va_start(args, msg);
+			vsnprintf(reinterpret_cast<char*>(buffer.data), sizeof(buffer.data), msg, args);
+			va_end(args);
 			buffer.header.log_type = LOG_TRACE;
 			// TODO: set date time in header
 			write(&buffer);
