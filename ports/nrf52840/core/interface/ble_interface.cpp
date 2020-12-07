b218215185c9be8f29b2f6948e68116476e262a1
Joshua Booth <joshua@icoteq.com>
2020-12-07 15:16:50 +0000

    CG-2: rename BLE name to "GenTracker"

--- ports/nrf52840/core/interface/ble_interface.cpp
+++ ports/nrf52840/core/interface/ble_interface.cpp
@@ -12,7 +12,7 @@
 
 #define APP_BLE_CONN_CFG_TAG            1                                           /**< A tag identifying the SoftDevice BLE configuration. */
 
-#define DEVICE_NAME                     "Nordic_UART"                               /**< Name of device. Will be included in the advertising data. */
+#define DEVICE_NAME                     "GenTracker"                                /**< Name of device. Will be included in the advertising data. */
 #define NUS_SERVICE_UUID_TYPE           BLE_UUID_TYPE_VENDOR_BEGIN                  /**< UUID type for the Nordic UART Service (vendor specific). */
 
 #define APP_BLE_OBSERVER_PRIO           3                                           /**< Application's BLE observer priority. You shouldn't need to modify this value. */
@@ -344,8 +344,6 @@ void BleInterface::nrf_qwr_error_handler(uint32_t nrf_error)
 /**@snippet [Handling the data received over BLE] */
 void BleInterface::nus_data_handler(ble_nus_evt_t * p_evt)
 {
-    printf("%s():%u\r\n", __FUNCTION__, __LINE__);
-
     if (p_evt->type == BLE_NUS_EVT_RX_DATA)
     {
         NRF_LOG_DEBUG("Received data from BLE NUS. Writing data on UART.");
