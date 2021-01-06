ab3bd6a45d781e960c253e64ba83c11087b0e395
Joshua Booth <joshua@icoteq.com>
2021-01-06 15:45:51 +0000

    interrupt_lock: improve compatibility with SDK

--- core/scheduling/interrupt_lock.hpp
+++ core/scheduling/interrupt_lock.hpp
@@ -1,8 +1,13 @@
 #pragma once
 
+#include <cstdint>
+
 class InterruptLock {
 public:
     InterruptLock();
     InterruptLock(const InterruptLock&) = delete;
     ~InterruptLock();
+
+private:
+    uint8_t m_nested;
 };
--- ports/nrf52840/core/scheduling/interrupt_lock.cpp
+++ ports/nrf52840/core/scheduling/interrupt_lock.cpp
@@ -1,28 +1,21 @@
 #include "interrupt_lock.hpp"
 #include "app_util_platform.h"
 
-#ifndef SOFTDEVICE_PRESENT
-static uint32_t g_in_critical_region = 0;
-#endif
-
 InterruptLock::InterruptLock()
 {
 #ifdef SOFTDEVICE_PRESENT
     m_nested = 0;
-    sd_nvic_critical_region_enter(&m_nested);
+    app_util_critical_region_enter(&m_nested);
 #else
-    __disable_irq();
-    g_in_critical_region++;
+    app_util_critical_region_enter(NULL);
 #endif
 }
 
 InterruptLock::~InterruptLock()
 {
 #ifdef SOFTDEVICE_PRESENT
-    sd_nvic_critical_region_exit(m_nested);
+    app_util_critical_region_exit(m_nested);
 #else
-    g_in_critical_region--;
-    if (g_in_critical_region == 0)
-        __enable_irq();
+    app_util_critical_region_exit(0);
 #endif
 }
