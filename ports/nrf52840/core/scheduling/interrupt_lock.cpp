87641ad6564cd03ac5c549c9ba4bd5f233f78c12
Joshua Booth <joshua@icoteq.com>
2021-01-06 15:35:00 +0000

    interrupt_lock: fix compilation for nrf52 interrupt lock

--- ports/nrf52840/core/scheduling/interrupt_lock.cpp
+++ ports/nrf52840/core/scheduling/interrupt_lock.cpp
@@ -1,12 +1,28 @@
 #include "interrupt_lock.hpp"
 #include "app_util_platform.h"
 
+#ifndef SOFTDEVICE_PRESENT
+static uint32_t g_in_critical_region = 0;
+#endif
+
 InterruptLock::InterruptLock()
 {
-    CRITICAL_REGION_ENTER();
+#ifdef SOFTDEVICE_PRESENT
+    m_nested = 0;
+    sd_nvic_critical_region_enter(&m_nested);
+#else
+    __disable_irq();
+    g_in_critical_region++;
+#endif
 }
 
 InterruptLock::~InterruptLock()
 {
-    CRITICAL_REGION_EXIT();
+#ifdef SOFTDEVICE_PRESENT
+    sd_nvic_critical_region_exit(m_nested);
+#else
+    g_in_critical_region--;
+    if (g_in_critical_region == 0)
+        __enable_irq();
+#endif
 }
