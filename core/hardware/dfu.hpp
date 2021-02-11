6ac93b29afd47c92aad478a0f2bff6756f786a32
Liam Wickins <liam@icoteq.com>
2021-02-11 17:12:19 +0000

    CG-151: adapted DFU class to allow the source address in ext flash to be passed to avoid hardcoding.

--- core/hardware/dfu.hpp
+++ core/hardware/dfu.hpp
@@ -5,5 +5,5 @@
 class DFU {
 public:
 	static void initialise();
-	static void write_ext_flash_dfu_settings(uint32_t image_size, uint32_t crc);
+	static void write_ext_flash_dfu_settings(uint32_t src_addr, uint32_t image_size, uint32_t crc);
 };
--- tests/mocks/mock_dfu.cpp
+++ tests/mocks/mock_dfu.cpp
@@ -7,7 +7,7 @@ void DFU::initialise()
 	mock().actualCall("initialise");
 }
 
-void DFU::write_ext_flash_dfu_settings(uint32_t image_size, uint32_t crc)
+void DFU::write_ext_flash_dfu_settings(uint32_t src_addr, uint32_t image_size, uint32_t crc)
 {
-	mock().actualCall("write_ext_flash_dfu_settings").withParameter("image_size", image_size).withParameter("crc", crc);
+	mock().actualCall("write_ext_flash_dfu_settings").withParameter("src_addr", src_addr).withParameter("image_size", image_size).withParameter("crc", crc);
 }
