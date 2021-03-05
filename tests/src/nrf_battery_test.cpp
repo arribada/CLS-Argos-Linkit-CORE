83cd77afda5208c782eded1562d0812965b69b15
Joshua Booth <joshua@icoteq.com>
2021-03-05 15:25:57 +0000

    CG-214: update nrf_battery_test.cpp to use new ADC references

--- tests/src/nrf_battery_test.cpp
+++ tests/src/nrf_battery_test.cpp
@@ -19,13 +19,13 @@ TEST_GROUP(NrfBattery)
 TEST(NrfBattery, CheckBatteryMon_CHEM_S18650_2600) {
 	NrfBatteryMonitor batt_mon(BSP::ADC::ADC_CHANNEL_0, BATT_CHEM_S18650_2600);
 	batt_mon.start();
-	SAADC::set_adc_value(5783);
+	SAADC::set_adc_value(9088);
 	CHECK_EQUAL(3993, batt_mon.get_voltage());
 	CHECK_EQUAL(77, batt_mon.get_level());
-	SAADC::set_adc_value(6783);
+	SAADC::set_adc_value(10660);
 	CHECK_EQUAL(4684, batt_mon.get_voltage());
 	CHECK_EQUAL(100, batt_mon.get_level());
-	SAADC::set_adc_value(1000);
+	SAADC::set_adc_value(1572);
 	CHECK_EQUAL(690, batt_mon.get_voltage());
 	CHECK_EQUAL(0, batt_mon.get_level());
 	batt_mon.stop();
@@ -34,13 +34,13 @@ TEST(NrfBattery, CheckBatteryMon_CHEM_S18650_2600) {
 TEST(NrfBattery, CheckBatteryMon_BATT_CHEM_CGR18650_2250) {
 	NrfBatteryMonitor batt_mon(BSP::ADC::ADC_CHANNEL_0, BATT_CHEM_CGR18650_2250);
 	batt_mon.start();
-	SAADC::set_adc_value(5783);
+	SAADC::set_adc_value(9088);
 	CHECK_EQUAL(3993, batt_mon.get_voltage());
 	CHECK_EQUAL(83, batt_mon.get_level());
-	SAADC::set_adc_value(6783);
+	SAADC::set_adc_value(10660);
 	CHECK_EQUAL(4684, batt_mon.get_voltage());
 	CHECK_EQUAL(100, batt_mon.get_level());
-	SAADC::set_adc_value(1000);
+	SAADC::set_adc_value(1572);
 	CHECK_EQUAL(690, batt_mon.get_voltage());
 	CHECK_EQUAL(0, batt_mon.get_level());
 	batt_mon.stop();
@@ -50,13 +50,13 @@ TEST(NrfBattery, CheckBatteryMon_BATT_CHEM_CGR18650_2250) {
 TEST(NrfBattery, CheckBatteryMon_BATT_BATT_CHEM_NCR18650_3100_3400) {
 	NrfBatteryMonitor batt_mon(BSP::ADC::ADC_CHANNEL_0, BATT_CHEM_NCR18650_3100_3400);
 	batt_mon.start();
-	SAADC::set_adc_value(5783);
+	SAADC::set_adc_value(9088);
 	CHECK_EQUAL(3993, batt_mon.get_voltage());
 	CHECK_EQUAL(81, batt_mon.get_level());
-	SAADC::set_adc_value(6783);
+	SAADC::set_adc_value(10660);
 	CHECK_EQUAL(4684, batt_mon.get_voltage());
 	CHECK_EQUAL(100, batt_mon.get_level());
-	SAADC::set_adc_value(1000);
+	SAADC::set_adc_value(1572);
 	CHECK_EQUAL(690, batt_mon.get_voltage());
 	CHECK_EQUAL(0, batt_mon.get_level());
 	batt_mon.stop();
