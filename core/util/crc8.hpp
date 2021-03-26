1572a6849c50025427bf7430990ddad62fea6975
Liam Wickins <liam@icoteq.com>
2021-03-26 20:02:12 +0000

    CG-255: fix input buffer alignment issues with CRC8 generator

--- core/util/crc8.hpp
+++ core/util/crc8.hpp
@@ -6,9 +6,10 @@
 #include "bitpack.hpp"
 
 class CRC8 {
+public:
 	// Zero pad the start of the data buffer so the number of total bits becomes a multiple of 8
 	static std::string zeropad(std::string const & data, unsigned int total_bits) {
-		unsigned int remainder = total_bits % 8;
+		unsigned int remainder = (8 - (total_bits % 8)) % 8;
 		std::string buffer;
 
 		if (remainder == 0)
@@ -17,20 +18,22 @@ class CRC8 {
 			return buffer;
 		}
 		else
-		{	
+		{
 			buffer.assign((total_bits + remainder) / 8, 0);
-			unsigned int base_pos = remainder, i = 0;
+			unsigned int op_base_pos = remainder;
+			unsigned int ip_base_pos = 0;
+			unsigned char byte;
 			while (total_bits) {
 				unsigned int bits = std::min(8U, total_bits);
-				PACK_BITS(data[i], buffer, base_pos, bits);
 				total_bits -= bits;
-				i++;
+				EXTRACT_BITS(byte, data, ip_base_pos, bits);
+				PACK_BITS(byte, buffer, op_base_pos, bits);
 			}
 		}
 
 		return buffer;
 	}
-public:
+
 	static unsigned char checksum(std::string const & data, unsigned int total_bits) {
 	  unsigned int crc = 0;
 	  int j, idx;
--- tests/src/crc8_test.cpp
+++ tests/src/crc8_test.cpp
@@ -17,3 +17,26 @@ TEST(CRC8, SimpleCRC8Test)
 	unsigned char crc = CRC8::checksum(data, data.size()*8);
 	CHECK_EQUAL(0xF4, crc);
 }
+
+
+TEST(CRC8, ForcedAlignedCRC8Test)
+{
+	std::string data = "\x00\x00\x07\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\x93"s;
+	unsigned char crc = CRC8::checksum(data, 8*data.size());
+	CHECK_EQUAL(0xC4, crc);
+}
+
+TEST(CRC8, MisalignedCRC8Test)
+{
+	std::string data = "\x00\x00\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xF2\x75\x4F\xD8"s;
+	unsigned char crc = CRC8::checksum(data, 91);
+	CHECK_EQUAL(0xC4, crc);
+}
+
+TEST(CRC8, ZeroPadTest)
+{
+	std::string data = "\x00\x00\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xF2\x75\x4F\xD8"s;
+	std::string expected = "\x00\x00\x07\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\x93"s;
+	std::string padded = CRC8::zeropad(data, 91);
+	CHECK_EQUAL(expected, padded);
+}
